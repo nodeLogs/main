cef1ae66ed00230b0fbfe13c58af4f3010108c56
nodeLogs <tsm.artem@gmail.com>
2022-02-17 13:42:34 +0300

    upd

--- nodelogo.sh
+++ nodelogo.sh
@@ -1,3 +1,12 @@
+#! /usr/bin/env bash
+
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+echo -e "${RED}This is some red text, ${ENDCOLOR}"
+echo -e "${GREEN}And this is some green text${ENDCOLOR}"
+
 #!/bin/bash
 
 echo -e "░██▀█░░██░░░░██████░░░░██████▄░░░░████████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"
