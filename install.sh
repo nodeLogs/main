2e834995038e92d098d9499104bbf310d4450de1
nodeLogs <tsm.artem@gmail.com>
2022-02-17 13:40:20 +0300

    upd

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+#! /usr/bin/env bash
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
 curl -s https://raw.githubusercontent.com/nodeLogs/main/main/nodelogo.sh | bash
 echo "Устанавливаем необходимый софт"
 curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_ufw.sh | bash &>/dev/null
 curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_rust.sh | bash &>/dev/null
 source ~/.cargo/env
 sleep 1
-echo "Весь необходимый софт установлен, копируем репозиторий"
+echo "${GREEN}Весь необходимый софт установлен, копируем репозиторий${ENDCOLOR}"
 git clone https://github.com/penumbra-zone/penumbra
 cd $HOME/penumbra && git checkout 005-mneme
 echo "Репозиторий успешно склонирован, начинаем билд"
