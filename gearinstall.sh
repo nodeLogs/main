b805d4b8cf17274c33b465a88c16f3207a363afc
nodeLogs <tsm.artem@gmail.com>
2022-03-01 16:46:01 +0300

    Update gearinstall.sh

--- gearinstall.sh
+++ gearinstall.sh
@@ -11,7 +11,7 @@ sleep 5
 echo -e "${PURPLE}>> Устанавливаем WASM${ENDCOLOR}"
 rustup toolchain add nightly
 rustup target add wasm32-unknown-unknown --toolchain nightly
-echo -e "${GREEN}Успешно${ENDCOLOR}"
+echo -e "${GREEN}/// Успешно${ENDCOLOR}"
 sleep 1
 echo -e "${PURPLE}>> Скачиваем бинарные файлы${ENDCOLOR}"
 sleep 3
@@ -19,27 +19,27 @@ wget https://builds.gear.rs/gear-nightly-linux-x86_64.tar.xz && \
 tar xvf gear-nightly-linux-x86_64.tar.xz && \
 rm gear-nightly-linux-x86_64.tar.xz && \
 chmod +x gear-node
-echo -e "${GREEN}Успешно${ENDCOLOR}"
+echo -e "${GREEN}/// Успешно${ENDCOLOR}"
 sleep 1
 echo -e "${PURPLE}>> Устанавливаем MAKE${ENDCOLOR}"
 sleep 3
 apt install make
-echo -e "${GREEN}Успешно${ENDCOLOR}"
+echo -e "${GREEN}/// Успешно${ENDCOLOR}"
 sleep 1
 echo -e "${PURPLE}>> Клонируем репозиторий${ENDCOLOR}"
 sleep 3
 git clone https://github.com/gear-tech/gear.git
 cd gear
-echo -e "${GREEN}Успешно${ENDCOLOR}"
+echo -e "${GREEN}/// Успешно${ENDCOLOR}"
 sleep 1
 echo -e "${PURPLE}>> Компилируем репозиторий. Потребуется больше вермени!${ENDCOLOR}"
 sleep 3
-echo -e "${GREEN}Успешно${ENDCOLOR}"
+make node-release
+echo -e "${GREEN}/// Успешно${ENDCOLOR}"
 sleep 1
 echo -e "${PURPLE}>> Создаем сервисный файл и конфиг${ENDCOLOR}"
 sleep 1
 cd /etc/systemd/system 
 touch gear-node.service
 echo -e "${GREEN}Установка успешна завершена. Следуйте дальнейшим командам из гайда.${ENDCOLOR}"
-sleep 5
 exit
