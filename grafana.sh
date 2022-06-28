a766d4e1ffef0c2b59bbdff38bccd51649ac78cc
nodeLogs <tsm.artem@gmail.com>
2022-06-28 16:44:32 +0300

    Create grafana.sh

---
+++ grafana.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+RED="\e[31m"
+GREEN="\e[32m"
+PURPLE="\e[36m"
+ENDCOLOR="\e[0m"
+
+sleep 2
+echo -e "${PURPLE}---------------------------------------${ENDCOLOR}"
+echo -e "${PURPLE}>> Installing node_exporter...${ENDCOLOR}"
+echo -e "${PURPLE}---------------------------------------${ENDCOLOR}"
+echo -e "${PURPLE}by lateful${ENDCOLOR}"
+sleep 2
+sudo apt-get install -y gnupg2 curl software-properties-common
+sleep 1
+curl https://packages.grafana.com/gpg.key | sudo apt-key add -
+sleep 1
+sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
+sleep 1
+sudo apt-get update
+sleep 1
+sudo apt-get -y install grafana
+sleep 1
+sudo systemctl enable --now grafana-server
+sleep 1
+echo -e "${PURPLE}>> node_exporter has been successfully installed${ENDCOLOR}"
